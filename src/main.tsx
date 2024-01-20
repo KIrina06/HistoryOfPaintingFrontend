1e03161858b6d4b1c92e3aee7f6a196076149211
KIrina06 <kirisha06@yandex.ru>
2024-01-20 06:55:27 +0300

    setup gh-pages

--- src/main.tsx
+++ src/main.tsx
@@ -1,21 +1,22 @@
-import {BrowserRouter, Route, Routes} from "react-router-dom";
+import {Route, Routes} from "react-router-dom";
 import Paintings from "./components/PaintingsPage.tsx";
 import Painting from "./components/PaintingPage.tsx";
 import ReactDOM from "react-dom/client";
 import { Navbar } from "./components/NavBar.tsx";
 import BreadCrumbs from "./components/Breadcrumbs.tsx";
+import { HashRouter as Router } from "react-router-dom";
 
 const root = ReactDOM.createRoot(
     document.getElementById('root') as HTMLElement
 );
 
 root.render(
-    <BrowserRouter>
+    <Router>
     <Navbar />
     <BreadCrumbs />
             <Routes>
                 <Route path="paintings/" element={<Paintings/>}/>
                 <Route path="paintings/:id" element={<Painting/>} />
             </Routes>
-    </BrowserRouter>
+    </Router>
 );
